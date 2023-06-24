07ddf7ed7800280e507adafd66417e86aff6d5f2
方君 <zzz895000780@163.com>
2023-06-24 19:17:42 +0800

    'feat(后台):框架提交'

---
+++ src/components/MarkdownEditor/default-options.js
@@ -0,0 +1,10 @@
+// doc: https://nhnent.github.io/tui.editor/api/latest/ToastUIEditor.html#ToastUIEditor
+export default {
+  minHeight: '200px',
+  previewStyle: 'vertical',
+  useCommandShortcut: true,
+  useDefaultHTMLSanitizer: true,
+  usageStatistics: false,
+  hideModeSwitch: false,
+  toolbarItems: [['heading', 'bold', 'italic', 'strike'], ['hr', 'quote'], ['ul', 'ol', 'task', 'indent', 'outdent'], ['table', 'image', 'link'], ['code', 'codeblock']]
+}
